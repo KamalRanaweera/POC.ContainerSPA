2e68f6076065539502f9102ef95c5dd42549dfff
Kamal Ranaweera <ranaweer@ualberta.ca>
2024-11-10 10:10:08 -0700

    Added a POC to pass on router instance and use it to navigate to an existing view from a component.

--- app-component-b/lib/AppComponentB.vue
+++ app-component-b/lib/AppComponentB.vue
@@ -1,6 +1,26 @@
+<script setup lang="ts">
+import {Router} from 'vue-router';
+
+const props = defineProps<{
+    routerInstance?: Router | undefined
+}>();
+
+
+function navigateTo(path:string){
+    console.log('Navigating to: ', path)
+    props.routerInstance?.push(path);
+}
+
+
+props.routerInstance?.getRoutes().forEach(r => console.log(r.name?.toString));
+
+</script>
+
 <template>
     <h2>App Component B</h2>
     <div>
         This is the application component imported from @spa/app-component-b package.
     </div>
+    <br />
+    <a @click="navigateTo('/about')">About</a>
 </template>
--- spa-poc/src/views/ComponentBView.vue
+++ spa-poc/src/views/ComponentBView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { AppComponentB } from '@spa/app-component-b'
+import { useRouter } from 'vue-router';
 
 </script>
 
 <template>
-    <app-component-b />
+    <app-component-b :router-instance="useRouter()"/>
 </template>
